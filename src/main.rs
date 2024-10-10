15e1400eab8c0bc9aeabd4599e84b6d6309b7e6b
proartmateur <ennimandroid@gmail.com>
2024-10-10 02:38:28 -0600

    Feature(Router): clean dead code

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod langs;
 mod presentation;
 mod routes;
 
-use arq::arq::Arq;
 use config::infrastructure::fs_config_repository::FsConfigRepository;
 use presentation::cli_header::cli_header;
 use presentation::help::help;
@@ -30,26 +29,4 @@ fn main() {
         println!("Argument {}: {}", i, arg);
     }
 
-    /*___ ARGS ___ */
-
-    /* __ Load Arq __ */
-    /* 
-    let path = "myarqu.json";
-
-    match read_arq_json(path) {
-        Ok(arq_items) => {
-            println!("Successfully parsed Arq JSON: {:#?}", arq_items);
-            // Test the function with a valid short_option
-            if let Some(found_item) = find_arq_item_by_option(_arq, &args[1]) {
-                println!("Found item by short_option: {:#?}", found_item);
-            } else {
-                println!("Item not found.");
-            }
-        }
-        Err(e) => {
-            eprintln!("Failed to read Arq JSON: {}", e);
-            panic!("");
-        }
-    }
-    */
 }
